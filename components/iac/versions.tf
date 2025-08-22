8bd9789e8e6750e403290ed44fd4781e9536c874
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-22 12:54:05 +0400

    -

--- components/iac/versions.tf
+++ components/iac/versions.tf
@@ -1,11 +1,15 @@
 # The Terraform Module
 terraform {
 
-  # AWS Provider
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.32.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
     }
   }
 }
