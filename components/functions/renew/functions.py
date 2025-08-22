158d9f3f1caf03e6386ad8c291834feb01488060
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-22 09:28:51 +0400

    - Updted lambda

--- components/functions/renew/functions.py
+++ components/functions/renew/functions.py
@@ -1,7 +1,7 @@
 from kink import di
 from datetime import datetime
-import OpenSSL
-import time
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 
 
 def extract_secret(secret, project, key):
@@ -12,22 +12,17 @@ def extract_secret(secret, project, key):
     ).value["raw"]
 
 
-def check_certificate_expiry(certificate, days_before_expiration: int = 30):
+def check_certificate_expiry(certificate: bytes, days_before_expiration: int = 30):
     """
-    Check if certificate exists and needs renewal using pyOpenSSL
+    Check if certificate exists and needs renewal
     """
+
     try:
         # Parse certificate
-        cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, certificate)
-
-        # Get expiry date
-        expiry_bytes = cert.get_notAfter()
-        expiry_str = expiry_bytes.decode("ascii")
-
-        # Parse the date string (format: YYYYMMDDHHMMSSZ)
-        expiry_date = datetime.strptime(expiry_str, "%Y%m%d%H%M%SZ")
+        cert = x509.load_pem_x509_certificate(certificate, default_backend())
 
         # Calculate days until expiry
+        expiry_date = cert.not_valid_after
         days_remaining = (expiry_date - datetime.now()).days
 
         print(
