6a9b493ff9693897b84984d7c811348d728f2a0d
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-22 13:46:33 +0400

    -

--- components/iac/datasource.tf
+++ components/iac/datasource.tf
@@ -1,5 +1,7 @@
 
 data "aws_ecr_image" "renew_web_helios" {
   repository_name = local.constants.ecr.helios_web_renew.name
+  image_digest    = null
+  image_tag       = null
   most_recent     = true
 }
