caf796aa39f33c6ad4d5911803ccefa91826d683
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-24 18:00:16 +0400

    - Fixed typo in workflow name - Reactivated all resources

--- components/iac/datasource.tf
+++ components/iac/datasource.tf
@@ -1,8 +1,8 @@
 
-# # Get the latest image from ECR
-# data "aws_ecr_image" "renew_web_helios" {
-#   repository_name = local.constants.ecr.helios_web_renew.name
-#   image_digest    = null
-#   image_tag       = null
-#   most_recent     = true
-# }
+# Get the latest image from ECR
+data "aws_ecr_image" "renew_web_helios" {
+  repository_name = local.constants.ecr.helios_web_renew.name
+  image_digest    = null
+  image_tag       = null
+  most_recent     = true
+}
--- components/iac/scheduler.tf
+++ components/iac/scheduler.tf
@@ -3,32 +3,32 @@ resource "aws_scheduler_schedule_group" "certificates_renewal" {
   name = local.constants.scheduler.group_name
 }
 
-# resource "aws_scheduler_schedule" "helios_web_renewals" {
+resource "aws_scheduler_schedule" "helios_web_renewals" {
 
-#   name                = "schedule-helios-web-renewal"
-#   description         = "The schedule for renewing the SSL certificates for the Helios web Load Balancer."
-#   group_name          = aws_scheduler_schedule_group.certificates_renewal.name
-#   schedule_expression = "cron(0 9 ? * SUN#1 *)"
-#   state               = "ENABLED"
+  name                = "schedule-helios-web-renewal"
+  description         = "The schedule for renewing the SSL certificates for the Helios web Load Balancer."
+  group_name          = aws_scheduler_schedule_group.certificates_renewal.name
+  schedule_expression = "cron(0 9 ? * SUN#1 *)"
+  state               = "ENABLED"
 
-#   schedule_expression_timezone = "Indian/Mauritius"
+  schedule_expression_timezone = "Indian/Mauritius"
 
-#   flexible_time_window {
-#     mode = "OFF"
-#   }
+  flexible_time_window {
+    mode = "OFF"
+  }
 
-#   target {
-#     arn      = module.lambda_renew_web_helios.lambda_function_arn
-#     role_arn = module.iam_role_scheduler_renew_web_helios.iam_role_arn
+  target {
+    arn      = module.lambda_renew_web_helios.lambda_function_arn
+    role_arn = module.iam_role_scheduler_renew_web_helios.iam_role_arn
 
-#     retry_policy {
-#       maximum_retry_attempts = 0
-#     }
-#   }
+    retry_policy {
+      maximum_retry_attempts = 0
+    }
+  }
 
-#   depends_on = [
-#     aws_scheduler_schedule_group.certificates_renewal,
-#     module.lambda_renew_web_helios,
-#     module.iam_role_scheduler_renew_web_helios
-#   ]
-# }
+  depends_on = [
+    aws_scheduler_schedule_group.certificates_renewal,
+    module.lambda_renew_web_helios,
+    module.iam_role_scheduler_renew_web_helios
+  ]
+}
